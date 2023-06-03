2eb1a464dc1a76530adb5d88ca02ec9db67ccf11
nassirou <nassirou9993@gmail.com>
2023-06-03 20:51:06 +0100

    the copy and paste using python

---
+++ copy_and_paste.py
@@ -0,0 +1,7 @@
+import pyperclip
+
+pyperclip.copy('Hello this a copy by python code')
+
+pyperclip.paste()
+
+# after to run this code press ctrl+V to paste the copy 'Hello this a copy by python code'
